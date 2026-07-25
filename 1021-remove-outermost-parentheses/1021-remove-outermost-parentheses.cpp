e2732e678a22e3e154edca3ca58190c7dbe222ce
Aayushi Gupta <aayushigupta181@gmail.com>
2026-07-25 23:53:33 +0530

    Time: 0 ms (100%), Space: 8.8 MB (97.8%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string ans="";
+        int depth=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('){
+                if(depth>0){
+                    ans+=s[i];
+                }
+                depth++;
+            }
+            if(s[i]==')'){
+                depth--;
+                if(depth>0){
+                    ans+=s[i];
+                }
+            }
+
+        }
+        return ans;
+        
+    }
+};
